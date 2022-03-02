f1b38c9cbf2da44cca3e0d8f0c1089214f0c7ed2
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-03-01 19:31:46 -0600

    sandbox

--- Python/sandbox.py
+++ Python/sandbox.py
@@ -1,15 +1,6 @@
-import string
+from typing import List
 
-nums = [num for num in range(1, 27)]
-alphabet = [letter for letter in string.ascii_lowercase]
-alphabet_nums_dict = dict(zip(nums, alphabet))
+def foo(lst: List[int]) -> str:
+    return 'hi'
 
-text = "The sunset sets at twelve o' clock."
-position_str = ''
-
-for letter in text:
-    for k, v in alphabet_nums_dict.items():
-        if v == letter.lower():
-            position_str += ' ' + str(k)
-
-print(position_str)
+print(foo([1,2,3]))
